b077cace44ff186e9d59cb49c57afe1d4cdee8dd
Niall1234 <robertniallross@gmail.com>
2026-03-21 17:38:30 +0000

    have added to indonesian_daily

--- indonesian_daily.py
+++ indonesian_daily.py
@@ -1,1 +1,37 @@
+import os
 import requests
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# setting roundup email variables
+text = """"""
+
+
+
+# news API 
+TOKEN = os.getenv("NEWS_API_KEY")
+
+country = "id"
+
+url = "https://api.thenewsapi.com/v1/news/top"
+
+params = {
+    "api_token": TOKEN,
+    "language": country,
+    "limit": "3",
+    "locale": country,
+    "published_on": datetime.now().strftime('%Y-%m-%d')
+}
+
+response_json = requests.get(url, params=params).json()
+
+text += "NEWS\n\n"
+for object in response_json["data"]:
+    text += f"Title: {object["title"]}\n"
+    text += f"Desc: {object["description"]}\n"
+    text += f"URL: {object["url"]}\n"
+    text += f"Published at: {object["published_at"]}\n\n"
+
+print(text)
