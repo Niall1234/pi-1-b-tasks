0f9aa7ff802bab2b6503c73dc58a1ed45fe22fd3
Niall1234 <robertniallross@gmail.com>
2026-04-11 14:56:15 +0100

    updated news limit in API call

--- indonesian_news.py
+++ indonesian_news.py
@@ -17,11 +17,13 @@ def get_news():
     url = "https://api.thenewsapi.com/v1/news/top"
 
     params = {
+        
         "api_token": TOKEN,
         "language": country,
-        "limit": "3",
+        "limit": "5",
         "locale": country,
         "published_on": datetime.now().strftime('%Y-%m-%d')
+
     }
 
     response_json = requests.get(url, params=params).json()
@@ -34,4 +36,6 @@ def get_news():
         text += f"Published at: {object["published_at"]}\n\n"
 
     return text
+
+print(get_news())
 
